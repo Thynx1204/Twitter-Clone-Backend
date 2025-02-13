589594854ea6f38e2bf27dc947c15ab0f6920375
AWEHA Nicolas <awenico98@gmail.com>
2025-02-13 11:16:57 +0100

    feat: initialize server

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import 'module-alias/register'
+import 'dotenv/config'
+import validateEnv, { DEFAULT_ENV } from './utils/validateEnv'
+import App from './app'
+import AuthController from './auth/auth.controller'
+
+validateEnv()
+
+const app = new App([new AuthController()], Number(process.env.PORT ?? DEFAULT_ENV.PORT))
+app.listen()
