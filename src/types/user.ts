06f8f852f0a24dc2820fa3402848bd9e0afa9af6
AWEHA Nicolas <awenico98@gmail.com>
2025-02-14 18:43:27 +0100

    refactor: remove profile property to UserInterface

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,9 +1,8 @@
-import { UserDtoType, ProfileDtoType } from '../auth/register.dto'
+import { UserDtoType } from '../auth/register.dto'
 import { Profile } from '@prisma/client'
 
 export interface UserInterface {
   user: UserDtoType
-  profile: ProfileDtoType
 }
 
 export type UserResponse = { email: string } & Profile
